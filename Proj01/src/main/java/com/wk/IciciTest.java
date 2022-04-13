bce5abf432b2e99151cbdac90b630e488d6b59d7
himan <toreachmanas@gmail.com>
2022-04-14 01:08:03 +0530

    commit by wakil kumar@@12AM

---
+++ Proj01/src/main/java/com/wk/IciciTest.java
@@ -0,0 +1,13 @@
+package com.wk;
+
+/**
+ * Hello world!
+ *
+ */
+public class IciciTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello ICICI World!" );
+    }
+}
