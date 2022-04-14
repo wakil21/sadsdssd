e2852eb84d576a5d1ffc53a4712051660d321fc0
himan <toreachmanas@gmail.com>
2022-04-14 08:42:52 +0530

    commit to local

--- Proj01/src/main/java/com/wk/IciciTest.java
+++ Proj01/src/main/java/com/wk/IciciTest.java
@@ -8,7 +8,7 @@ public class IciciTest
 {
     public static void main( String[] args )
     {
-    	System.out.println( "Hello wakil kumar World!" );
+    
     	System.out.println( "Hello ICICI World!" );
     	System.out.println("IciciTest.main()");
     	System.out.println( "-----------------------------------" );
