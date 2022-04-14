2e1f6fbe0e05d802914c734b3aec3ffefb7432e5
himan <toreachmanas@gmail.com>
2022-04-14 09:46:40 +0530

    add by wk 9:46 am

--- Proj01/src/main/java/com/wk/IciciTest.java
+++ Proj01/src/main/java/com/wk/IciciTest.java
@@ -12,7 +12,7 @@ public class IciciTest
     	System.out.println("sri ganesh");
     	System.out.println( "Hello ICICI World!" );
     	System.out.println("IciciTest.main()");
-    	System.out.println("tatatat");
+    	System.out.println("tatatat=====>121212121");
     	System.out.println( "-----------------------------------" );
     	System.out.println( "Hello ICICI World222qwewewe" );
     }
