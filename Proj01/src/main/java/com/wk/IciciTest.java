bdec066a93c6ac9864a06766663de178a55cf8b9
himan <toreachmanas@gmail.com>
2022-04-14 09:09:57 +0530

    updated by wakil 9 am

--- Proj01/src/main/java/com/wk/IciciTest.java
+++ Proj01/src/main/java/com/wk/IciciTest.java
@@ -10,7 +10,8 @@ public class IciciTest
     {
     
     	System.out.println("sri ganesh");
-    	
+    	System.out.println( "Hello ICICI World!" );
+    	System.out.println("IciciTest.main()");
     	System.out.println( "-----------------------------------" );
     	System.out.println( "Hello ICICI World222qwewewe" );
     }
