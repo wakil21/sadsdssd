1c40afd8735b60e13c2966a770ccb8a93e274c7c
himan <toreachmanas@gmail.com>
2022-04-14 08:48:05 +0530

    started new

--- Proj01/src/main/java/com/wk/IciciTest.java
+++ Proj01/src/main/java/com/wk/IciciTest.java
@@ -9,6 +9,7 @@ public class IciciTest
     public static void main( String[] args )
     {
     
+    	System.out.println("sri ganesh");
     	System.out.println( "Hello ICICI World!" );
     	System.out.println("IciciTest.main()");
     	System.out.println( "-----------------------------------" );
