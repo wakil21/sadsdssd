9f9ac2b2ad5d218f8d8752e881ba004de475313c
himan <toreachmanas@gmail.com>
2022-04-14 07:37:41 +0530

    wakil kumar

--- Proj01/src/main/java/com/wk/IciciTest.java
+++ Proj01/src/main/java/com/wk/IciciTest.java
@@ -10,6 +10,6 @@ public class IciciTest
     {
     	System.out.println( "Hello wakil World!" );
     	System.out.println( "Hello ICICI World!" );
-    	System.out.println( "Hello ICICI World222" );
+    	System.out.println( "Hello ICICI World222qwewewe" );
     }
 }
