6667ffc1b14fe24880f29067502fd7103aa69cbf
himan <toreachmanas@gmail.com>
2022-04-14 09:12:16 +0530

    again added new line by wakil

--- Proj01/src/main/java/com/wk/IciciTest.java
+++ Proj01/src/main/java/com/wk/IciciTest.java
@@ -12,6 +12,7 @@ public class IciciTest
     	System.out.println("sri ganesh");
     	System.out.println( "Hello ICICI World!" );
     	System.out.println("IciciTest.main()");
+    	System.out.println("tatatat");
     	System.out.println( "-----------------------------------" );
     	System.out.println( "Hello ICICI World222qwewewe" );
     }
